8d085e3874e6896287d11bc94f802b1d1752e038
luis <foreverluiscastro@gmail.com>
2021-06-21 10:15:44 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,19 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    if params[:name]
+      
+      students = Student.all.select{ |s| s.first_name == params[:name] || s.last_name == params[:name]}
+      students = Student.where("first_name == params[:name]").merge(Student.where("last_name == params[:name]"))
+      binding.pry
+    else
+      students = Student.all
+    end
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
 end
